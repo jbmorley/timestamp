320a4c0267015289993615c7e763d9ae0e3309a3
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-06-12 07:29:26 +0200

    Initial commit of the render time command line utility.

---
+++ Podfile
@@ -0,0 +1,3 @@
+platform :osx, '10.9'
+
+pod 'JVArgumentParser', '~> 0.2'
---
+++ render-time/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  render-time
+//
+//  Created by Jason Barrie Morley on 12/06/2014.
+//  Copyright (c) 2014 Jason Barrie Morley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
